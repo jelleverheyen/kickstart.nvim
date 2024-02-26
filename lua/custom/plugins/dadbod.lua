d33d0d9a2ba32e3550c346352fa50d6cb53abcb7
Jelle Verheyen <jelle_verheyen@crgl-thirdparty.com>
2024-02-26 17:15:14 +0100

    fix(sql): Fix SQL completion

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,5 +1,27 @@
+local function db_completion()
+  require('cmp').setup.buffer { sources = {{ name = 'vim-dadbod-completion' }} }
+end
+
 return {
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-ui',
-  'kristijanhusak/vim-dadbod-completion',
+  'kristijanhusak/vim-dadbod',
+  dependencies = { 'kristijanhusak/vim-dadbod-completion', 'kristijanhusak/vim-dadbod-ui' },
+  config = function ()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        "sql",
+      },
+      command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        'sql',
+        'mysql',
+        'plsql'
+      },
+      callback = function()
+        vim.schedule(db_completion)
+      end,
+    })
+  end
 }
