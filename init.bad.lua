fff806c035da3d32889acf2dcf223a30d1d5d35f
Jelle Verheyen <jelle_verheyen@crgl-thirdparty.com>
2024-03-12 08:31:29 +0100

    Update to newest Kickstart version

--- init.bad.lua
+++
@@ -1,8 +0,0 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require('jelle')
